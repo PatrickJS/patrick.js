c3f50a01c2b4c56d929abe5dcf0e6c069f21a9e5
gdi2290 <github@gdi2290.com>
2014-01-19 17:17:32 -0800

    add live reload to gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,10 @@
 require('longjohn');
-var gulp = require('gulp');
-var karma = require('gulp-karma');
+var gulp       = require('gulp');
+var karma      = require('gulp-karma');
+var refresh    = require('gulp-livereload');
+var livereload = require('tiny-lr');
+
+var server = livereload();
 
 var testFiles = [
   'index.js',
@@ -13,6 +17,16 @@ gulp.task('test', function() {
       configFile: 'test/karma.conf.js',
       action: 'run'
     }));
+});
+
+gulp.task('pjs', function(cb) {
+
+  gulp.watch(testFiles, function(event) {
+    return gulp.src(testFiles)
+      .pipe(refresh(server));
+
+  });
+
 });
 
 gulp.task('default', function() {
